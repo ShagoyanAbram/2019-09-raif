5b7765b8a00473b149ac0d2d24d751a8d731b679
abram <abramshagoyan@gmail.com>
2019-10-13 21:35:25 +0600

    ДОМАШНЕЕ ЗАДАНИЕ БЛОК 3. ЗАНЯТИЕ 3. ТЕМА «АННОТАЦИИ В JAVA» Упражнение 1(объединено с Домашней работой 2- Reflection).

---
+++ HomeWorkResults/Abram-Shagoyan/java-core/src/main/java/com/abram/special_tools/annotation/atm/CurrentAccount.java
@@ -0,0 +1,12 @@
+package com.abram.special_tools.annotation.atm;
+
+import com.abram.special_tools.annotation.atm.service.impl.CurrentService;
+
+public class CurrentAccount extends Accounts {
+    public CurrentAccount() {
+    }
+
+    public CurrentAccount(int accountNumber, double balance) {
+        super(accountNumber, balance, AccountType.CURRENT, new CurrentService());
+    }
+}
